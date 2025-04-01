5b24277a055f32ca6984b3841b7dd287c659180a
Alec Campos Aoki <alec.campsaoki@gmail.com>
2025-04-01 17:24:27 -0300

    Update comments

--- src/main.asm
+++ src/main.asm
@@ -1,7 +1,7 @@
 	.data # stores data in RAM
 	.align 0 # aligns data by byte
-	# defines character of each feature
-features:
+	# defines character of each functionality
+functionalities:
 	# sum, sub, mul, div, undo, finish 
 	.ascii "+", "-", "*", "/", "u", "f"
 
@@ -9,7 +9,7 @@ features:
 	.align 2 # instructions aligned by word
 	.globl main # sets main as program start
 main:
-	# defines registers to store functionality
+	# defines registers to store functionalities (s0 - s5)
 	la t0, features # loads 1st byte address 
 	lb s0, 0(t0) # s0 = '+'
 	lb s1, 1(t0) # s1 = '-'
@@ -18,40 +18,31 @@ main:
 	lb s4, 4(t0) # s4 = 'u'
 	lb s5, 4(t0) # s5 = 'f'
 	
-	# create list(returns the address of the list in a0)
+	# create list (returns the address of the list in a0)
 	jal list
-	add s6, zero, a0 # s6 = list pointer
+	add s6, zero, a0 # s6 = pointer to list
 	
-	# read 1st input - number
-	li a7, 5 # command 5: read integer
-	ecall # syscall to read (a0 = input)
-	add t0, zero, a0 # save input 
-	
-	# push first element to the list
-	add a0, zero, s6 # set list as argument
-	add a1, zero, t0 # set value to be enter
-	jal list_push # function call
-	
-loop:
-	# read char and integer
-
+	# TODO: read 1st input
 
 # Function that creates a list
 # a0: returns address from list
 list:
 	# control structure consists of
-	# on a pointer to the first node
-	li a7, 9 # command 9: allocate memory on heap
+	# a pointer to the first node
+	li a7, 9 # instruction 9: allocate memory on heap
 	li a0, 4 # size to be allocated - 1 pointer = 4 bytes
 	ecall # syscall to allocate memory on the heap
-	sw zero, 0(a0) # set pointer to NULL(= 0)
-	jr ra # returns to the calling address
-	
+
+	sw zero, 0(a0) # set pointer to NULL(0)
+	jr ra # jump to return address
+
+# TODO
 # Function that inserts an element into the list
 # a0: list address
 # a1: value to be entered
 list_push:
 
+# TODO
 # Function to test list
 # print -1 if list dont exist
 # print elements in the list
